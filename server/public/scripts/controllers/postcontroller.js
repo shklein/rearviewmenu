fb42ffa08efae639db1ac5618fc054b0a36609a3
Samantha Klein <saradurga@gmail.com>
2016-09-08 13:12:15 -0500

    still working

--- server/public/scripts/controllers/postcontroller.js
+++ server/public/scripts/controllers/postcontroller.js
@@ -3,10 +3,12 @@ myApp.controller('PostController', ['$scope', '$http',  function($scope, $http)
 
 $scope.sources = ["Cookbook", "Magazine", "URL", "other"];
 $scope.currentRec = {};
+$scope.currentRecDate = {};
 $scope.message = false;
 
 $scope.submitCurrentRec = function () {
   $scope.currentRec.source = $scope.selectedSource;
+  $scope.currentRec.date_made = $scope.currentRecDate;
     var data = $scope.currentRec;
 
 
@@ -15,6 +17,7 @@ $scope.submitCurrentRec = function () {
         console.log('Recipe saved.');
         $scope.message = true;
         $scope.currentRec = {};
+        $scope.currentRecDate = {};
         $scope.selectedSource = 0;
       } else {
         console.log('Error:', response.data);
